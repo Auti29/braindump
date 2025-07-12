f313e196dcab9da880664171fd12475df1b094fd
achyutauti30@gmail.com <achyutauti30@gmail.com>
2025-07-13 03:49:19 +0530

    init-2

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import { UserModel } from "./db";
+import {userMiddleware} from "./middleware"
+import jwt from 'jsonwebtoken';
 const app = express();
 // const router = express.Router();
 dotenv.config();
 const PORT = process.env.BE_PORT;
 const DB_URL = process.env.DB_URL;
+const JWT_SECRET = process.env.JWT_SECRET;
+
 app.use(express.json());
 
-app.post('/api/v1/user/signup', async (req, res) => {
+app.post('/api/v1/signup', async (req, res) => {
     // todo: zod
     const { username, password } = req.body;
 
@@ -46,22 +50,63 @@ app.post('/api/v1/user/signup', async (req, res) => {
     }
     
 });
-app.post('api/v1/user/signin', (req, res) => {
+
+
+app.post('/api/v1/signin', async (req, res) => {
+    const {username, password} = req.body;
+    if(!username || !password) {
+        return res.status(404).json({
+            message: "wrong credentials!"
+        })
+    }   
+    try{
+        const user = await UserModel.findOne({username});
+        if(!user){
+            return res.status(404).json({
+            message: "wrong credentials!"
+        });
+        }
+
+        const isPassCorrect = await bcrypt.compare(password, user.password);
+        if(!isPassCorrect){
+                        return res.status(404).json({
+            message: "wrong credentials!"
+        });
+
+        }
+
+        const token = jwt.sign({
+            id: user._id,
+            username: user.username
+        }, JWT_SECRET!)
+
+        return res.status(200).json({
+            msg: "Login successful",
+            token,
+        });
+
+    }catch(err){
+        console.log("Signin error");
+
+        return res.status(411).json({
+            msg: "Something went wrong",
+            error: err
+        });
+    }
+});
+app.get('/api/v1/content', userMiddleware, (req, res) => {
 
 });
-app.get('api/v1/user/content', (req, res) => {
-//protected route
+app.post('/api/v1/content', (req, res) => {
+
 });
-app.post('api/v1/user/content', (req, res) => {
-// protected route
+app.delete('/api/v1/content', (req, res) => {
+
 });
-app.delete('api/v1/user/content', (req, res) => {
-// protected route
-});
-app.post('api/v1/user/share', (req, res) => {
-// protected route
+app.post('/api/v1/share', (req, res) => {
+
 })
-app.get('api/v1/user/:shareLink' , (req, res) => {
+app.get('/api/v1/:shareLink' , (req, res) => {
 
 })
 
